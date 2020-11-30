acee134b112b15327014b8909ac361f951fdaabc
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 10:28:29 +0100

    homepage styling

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,28 @@ import './App.css'
 function Home() {
     const homePageImage = {
         objectFit: "cover",
-        opacity: "0.6",
+        opacity: "0.4",
         width: "100%",
     }
 
     return (
 
         <Fragment>
-            <div className="dark-overlay">
-                <img style={homePageImage} src={vegetationImage} />
+            <div className="start-page-image" >
+                <header className="dark-overlay">
+                    <img style={homePageImage} alt="homepage-vegetation" src={vegetationImage} />
+                    <section className="centered-text">
+                        <h3> hemsidor | design </h3>
+                        <h1 >Ullman IT Consulting</h1>
+                        <p>Välkommen! Vi hjälper dig marknadsföra dig själv eller ditt företag med en hemsida. Kom igång idag!</p>
+                        <button className="btn btn-success" > Tjänster</button>
+                        <button className="btn btn-success" > Om Oss</button>
+
+                    </section>
+
+                </header>
             </div>
+
         </Fragment>
     )
 }
