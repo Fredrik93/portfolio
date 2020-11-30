4df33d73ca9b194c439d78ab3ed3c95b503c462e
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 14:25:24 +0100

    500 commits

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Confetti from 'react-confetti'
 
 function Services() {
+
     return (
-        <h3>Services page</h3>
+        <div style={{ width: "200vh", textAlign: "center", marginTop: "10vh" }} >
+            <h2>Wooooh < br /> 500</h2>
+            <h1>Commits 2020!!</h1>
+            <Confetti />
+        </div>
     )
 }
+
+
 export default Services
