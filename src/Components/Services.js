f6277503dd2955c5adb1f1816c3e83797bb56202
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 15:38:10 +0100

    update

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -4,7 +4,7 @@ import Confetti from 'react-confetti'
 function Services() {
 
     return (
-        <div style={{ textAlign: "center", marginTop: "10vh" }} >
+        <div style={{ textAlign: "center", marginTop: "30vh" }} >
             <h2>Services offered page < br /> (keeping the confetti for a while because its pretty awesome)</h2>
             <Confetti />
         </div>
