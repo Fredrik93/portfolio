465056e557b8f4d6b192926c9b1b063e1b4a3df1
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 09:17:27 +0100

    fix image bug homepage

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react'
-
+import vegetationImage from './Assets/vegetation-1.jpg'
 function Home() {
     return (
-        <h3> Home page</h3>
+        <Fragment>
+            <img src={vegetationImage} />
+            <h3> Home page</h3>
+        </Fragment>
     )
 }
 export default Home
