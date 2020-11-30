2cac6ec02f17b7ec707561a861f8fe7239485a60
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 19:01:11 +0100

    add logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import '../Styles/App.css'
+import logo from '../Assets/logo-white.png'
 function NavBar() {
 
     return (
         <Navbar id="navbar" expand="lg" >
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home"> <img id="img-logo" alt="uic-logo" src={logo} /> </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
