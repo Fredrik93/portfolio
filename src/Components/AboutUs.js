a6d572139992d91d1d4235cbfc3c5f0c45bf4121
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-30 19:17:41 +0100

    add aboutus

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import '../Styles/AboutUs.css'
+import waterfallImage from '../Assets/waterfall.jpg'
 function AboutUs() {
     return (
-        <h3>About page</h3>
+        <div className="start-page-layout" >
+            <header className="dark-overlay">
+                <img className="homepage-image" alt="homepage-vegetation" src={waterfallImage} />
+                <section className="centered-text">
+                    <h1> Om Oss </h1>
+                </section>
+
+            </header>
+        </div>
+
     )
 }
 export default AboutUs
