113f23b59dda1274e9be3c51361a27f68914b86a
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-05 15:54:55 +0100

    add style

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,15 +4,15 @@ import TodosItem from './TodosItem'
 import data from './todosData'
 export class Todos extends React.Component {
 
-
-
     render() {
+
         const dataItems = data.map((item) => {
             return <TodosItem itemName={item.name} key={item.id} />
         })
         return (
-            <div>
+            <div >
                 <Header headerText="Todos Component" />
+                < br />
                 {dataItems}
             </div>
         )
--- src/Components/TodosItem.js
+++ src/Components/TodosItem.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function TodosItem(props) {
+    const styling = {
+        textAlign: "left",
+        background: "#eee",
+        width: "30%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        display: "block"
+    }
     return (
-        <h3>{props.itemName}</h3>
+        <h3 style={styling} > {props.itemName}</h3>
     )
 }
 export default TodosItem
