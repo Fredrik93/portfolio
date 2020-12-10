adab9304dd1d28a03a07a855b8a80c2581eb6fd5
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-10 11:07:00 +0100

    add images

--- src/Components/GalleryBlock.js
+++ src/Components/GalleryBlock.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import Image1 from '../Assets/in-the-woods.jpg'
-import Image2 from '../Assets/service3.jpg'
+import { Link } from 'react-router-dom'
+import Image1 from '../Assets/service3.jpg'
+import VermontLighter from '../Assets/vermont-lighter.jpg'
+import VermontLighter2 from '../Assets/vermont-lighter-2.jpg'
+import Inthewoods from '../Assets/in-the-woods.jpg'
+import paintRaspberry from '../Assets/paint_raspberry.jpg'
+import waterfall from '../Assets/waterfall.jpg'
+import Patrik from '../Assets/patrik.jpg'
+import raspberry from '../Assets/raspberry.jpg'
+import winter from '../Assets/winter.jpg'
+import vegetation from '../Assets/vegetation-1.jpg'
 
-const imageArray = [Image1, Image2]
 class GalleryBlock extends React.Component {
 
-    randomizeImages() {
-        const imgStyle = {
-            height: "auto",
-            padding: "1vh",
-            width: "25%"
-        }
-        let rand = 1
-        return < img style={imgStyle} id="galleryImages" src={imageArray[rand]} alt="galleryimage" />
-    }
 
     render() {
         const styleText = {
@@ -37,8 +36,23 @@ class GalleryBlock extends React.Component {
                 <hr style={{ width: "20%", backgroundColor: "black" }} />
                 <span>
                     <Container style={styleImages}  >
-                        {this.randomizeImages()}
+                        < img style={imgStyle} id="galleryImages" src={vegetation} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={raspberry} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={VermontLighter} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={VermontLighter2} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={Inthewoods} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={paintRaspberry} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={waterfall} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={Patrik} alt="galleryimage" />
+                        < img style={imgStyle} id="galleryImages" src={winter} alt="galleryimage" />
+
                     </Container>
+                    <Link to="/gallery">
+                        <button
+                            className="btn btn-lg "
+                            id="historyBtn" > Till Galleri
+                    </button>
+                    </Link>
 
                 </span>
             </div>
