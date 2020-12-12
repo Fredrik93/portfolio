1d0b62e4900c8f5fefa5f5a30970f16c18bb3b77
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-12 06:47:26 +0100

    add style

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Footer() {
+    const footerStyle = {
+        height: "20vh",
+        backgroundColor: "lightgray"
+    }
     return (
-        <div> footer </div>
+
+        <div style={footerStyle}> footer </div>
     )
 } export default Footer
