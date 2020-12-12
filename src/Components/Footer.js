869d928593eadfaeedb67f636e335076aa438cd1
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-12 07:31:13 +0100

    add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import logo from '../Assets/logo.png'
 
 function Footer() {
     const footerStyle = {
         height: "20vh",
-        backgroundColor: "lightgray"
+        backgroundColor: "lightgray",
+        display: "flex",
+
+
     }
     return (
 
-        <div style={footerStyle}> footer </div>
+        <div style={footerStyle}>
+            <span>
+                <img style={{ height: "auto", width: "20vh", marginLeft: "30vh", marginTop: "3vh" }} src={logo} />
+            </span>
+            <span style={{ marginLeft: "30vh" }} >
+                <p>Snabblänkar</p>
+
+            </span>
+            <span style={{ marginLeft: "30vh" }} >
+                <p>Information</p>
+
+            </span>
+        </div>
     )
 } export default Footer
