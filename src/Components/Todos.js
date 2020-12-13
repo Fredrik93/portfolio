fb2b73a524c025eb04efcf8789d1fc0547fa14e3
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-13 11:36:34 +0100

    add task function

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import Header from './Header'
-import TodosItem from './TodosItem'
-import data from './todosData'
-import { Button } from 'react-bootstrap'
 import vegetationImage from '../Assets/vegetation-1.jpg'
 import SubmitForm from './SubmitForm/SubmitForm'
+import TasksList from './TasksList/TasksList'
 
 export class Todos extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+        this.addTask = this.addTask.bind(this)
+        this.removeTask = this.removeTask.bind(this)
+    }
+    addTask(task) {
+        this.setState((prevState) => {
+            return {
+                tasks: prevState.tasks.concat(task)
+            }
+        })
+    }
     render() {
         return (
-            <div >
+            <div className="Todos" >
                 <Header headerText="Todos app" image={vegetationImage} />
                 <SubmitForm />
+                <TasksList tasks={this.state.tasks} removeTask={this.removeTask} />
             </div>
         )
     }
