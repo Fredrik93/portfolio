5c1cf63364fc949a17b8217d7a15103de90c170f
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-13 08:23:53 +0100

    start fresh todo app

---
+++ src/Components/SubmitForm/SubmitForm.js
@@ -0,0 +1,9 @@
+import './SubmitForm.css'
+
+export default function SubmitForm() {
+    return (
+        <div>
+            <p > <input type="submit" /> Submit </p>
+        </div>
+    )
+}
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,60 +4,14 @@ import TodosItem from './TodosItem'
 import data from './todosData'
 import { Button } from 'react-bootstrap'
 import vegetationImage from '../Assets/vegetation-1.jpg'
+import SubmitForm from './SubmitForm/SubmitForm'
 
 export class Todos extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            completed: '',
-            task: '',
-            todos: data
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.addTask = this.addTask.bind(this)
-    }
-    handleChange(id) {
-
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed
-                }
-                return todo
-            })
-            return {
-                todos: updatedTodos
-            }
-        })
-    }
-    addTask() {
-        this.setState((prevState) => {
-
-            const addedTodo = this.state.todos.push({ id: 3, name: 'clean bathroom' })
-            console.log(prevState)
-
-            return {
-                todos: addedTodo
-            }
-        })
-    }
     render() {
-
-        const dataItems = data.map((choreItem) => {
-
-            return <TodosItem
-                key={choreItem.id}
-                choreItem={choreItem}
-                handleChange={this.handleChange} />
-        })
         return (
             <div >
-                <Header headerText="Todos Component" image={vegetationImage} />
-                < br />
-                {dataItems}
-                <div style={{ textAlign: "center" }} >
-                    <Button variant="success" onClick={() => { this.addTask() }} > Add task</Button>
-                </div>
+                <Header headerText="Todos app" image={vegetationImage} />
+                <SubmitForm />
             </div>
         )
     }
