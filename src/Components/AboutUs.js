f2c8217c2f5409c13368e88873892da7aed1699d
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-14 14:07:49 +0100

    grids

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import runningWater from '../Assets/running-water.jpg'
+import selfie from '../Assets/selfie.jpg'
 import '../index.css'
 import Header from './Header'
 function AboutUs() {
@@ -9,9 +10,11 @@ function AboutUs() {
         <Fragment>
             <Header headerText="Om Oss" image={runningWater} />
             <Row  >
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman</Col>
+                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman <hr ></hr> </Col>
                 <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >text </Col>
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >Image</Col>
+                <Col style={{ backgroundColor: "green", border: "2px solid black", }} xs={12} sm={12} md={6} lg={6} >
+                    <img style={{ height: "auto", width: "100%" }} src={selfie} alt="selfie" />
+                </Col>
 
 
             </Row>
