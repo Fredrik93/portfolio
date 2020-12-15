25d78f8f79d5b21eda3138bc9a25adedc2e29ba3
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-15 14:33:33 +0100

    grid system and text

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -9,12 +9,38 @@ function AboutUs() {
     return (
         <Fragment>
             <Header headerText="Om Oss" image={runningWater} />
-            <Row  >
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman <hr ></hr> </Col>
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >text </Col>
+            <Row style={{ marginTop: "10vh" }} >
+                <Col style={{ backgroundColor: "green", border: "2px solid black", fontSize: "3rem", textAlign: "center" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman <hr style={{ backgroundColor: "black" }} /> </Col>
+                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >empty fill </Col>
                 <Col style={{ backgroundColor: "green", border: "2px solid black", }} xs={12} sm={12} md={6} lg={6} >
                     <img style={{ height: "auto", width: "100%" }} src={selfie} alt="selfie" />
                 </Col>
+                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >
+                    <h4 style={{ fontWeight: "bold" }} > Studying Software engineering and management at the University
+                    of Gothenburg with 1+ years experience in programming,
+                          web development and project management </h4>
+                    <p>
+                        I really enjoyed my six years in the service industry working as a bartender and sales clerk, but iv’e always had a dream of running my own business. Something was missing and i couldn’t put my finger on what it was.
+
+                        I decided to take a 30 credit course in economics in 2018 and soon realized how much i value a university degree.
+
+                        What was missing was something to be passionate about, sure that there was motivation in offering the best service i could and improve my sales techniques but still felt like i needed something that allowed me to develop myself more.
+
+                        I started studying IT two years ago and It’s been the most difficult, frustrating headache-prone experience I’ve ever encountered.
+
+                        But it is also precisely what i wish to continue working with for the rest of my career, and life. For a neat website, a pretty algorithm or a well developed project-plan is that sense of fulfillment ive been searching for.
+
+                        I’m convinced that with my competences and solid communication, you and I will be able to develop a great product that we both can take pride in!
+
+                        Looking forward to hear from you!
+
+                    </p>
+                    <h4
+                        style={{ fontWeight: "bold" }} >
+                        Fredrik Ullman , Founder and Developer
+                        </h4>
+                </Col>
+
 
 
             </Row>
