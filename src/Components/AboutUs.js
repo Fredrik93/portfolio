25b05b7150be861ddb9c3d77f7ffc2c3f950d37b
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-16 11:00:53 +0100

    fix grid for about us page

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -5,23 +5,25 @@ import selfie from '../Assets/selfie.jpg'
 import '../index.css'
 import 'font-awesome/css/font-awesome.min.css';
 import Header from './Header'
+import cv from '../Assets/resume-2020.pdf'
 function AboutUs() {
 
     return (
         <Fragment>
             <Header headerText="Om Oss" image={runningWater} />
-            <Row style={{ marginTop: "10vh" }} >
-                <Col style={{ backgroundColor: "green", border: "2px solid black", fontSize: "3rem", textAlign: "center" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman <hr style={{ backgroundColor: "black" }} /> </Col>
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >empty fill </Col>
-                <Col style={{ backgroundColor: "green", border: "2px solid black", }} xs={12} sm={12} md={6} lg={6} >
-                    <img style={{ height: "auto", width: "100%", marginBottom: "2vh" }} src={selfie} alt="selfie" />
+            <Row style={{ marginTop: "10vh", }} >
+                <Col style={{ fontSize: "3rem", textAlign: "center" }} xs={12} sm={12} md={6} lg={6} >Fredrik Ullman <hr style={{ backgroundColor: "black", width: "50%" }} /> </Col>
+                <Col xs={12} sm={12} md={6} lg={6} > </Col>
+                <Col style={{ textAlign: "center" }} xs={12} sm={12} md={6} lg={6} >
+                    <img style={{ height: "auto", width: "60%", marginBottom: "2vh" }} src={selfie} alt="selfie" />
+                    < br />
                     <a target="_blank" href="https://github.com/Fredrik93"> <i className="fa fa-github" style={{ fontSize: "3rem", marginLeft: "1vh", color: "black", marginBottom: "2vh" }}></i> </a>
                     <a target="_blank" href="https://www.linkedin.com/in/carl-fredrik-ullman-919645174/"  > <i className="fa fa-linkedin" style={{ fontSize: "3rem", marginLeft: "1vh", color: "black" }}></i> </a>
-                    <a target="_blank" href="https://www.w3schools.com"  > <i className="far fa-file-upload" style={{ fontSize: "3rem", marginLeft: "1vh", color: "black" }}></i> </a>
+                    <a target="_blank" href={cv}  > <i className="fa fa-file" style={{ fontSize: "3rem", marginLeft: "1vh", color: "black" }}></i> </a>
 
                 </Col>
-                <Col style={{ backgroundColor: "green", border: "2px solid black" }} xs={12} sm={12} md={6} lg={6} >
-                    <h4 style={{ fontWeight: "bold" }} > Studying Software engineering and management at the University
+                <Col style={{ textAlign: "left", paddingRight: "15vh" }} xs={12} sm={12} md={6} lg={6} >
+                    <h4 style={{ fontWeight: "bold", marginBottom: "4vh" }} > Studying Software engineering and management at the University
                     of Gothenburg with 1+ years experience in programming,
                           web development and project management </h4>
                     <p>
@@ -41,7 +43,7 @@ function AboutUs() {
 
                     </p>
                     <h4
-                        style={{ fontWeight: "bold" }} >
+                        style={{ fontWeight: "bold", marginTop: "4vh" }} >
                         Fredrik Ullman , Founder and Developer
                         </h4>
                 </Col>
@@ -49,7 +51,7 @@ function AboutUs() {
 
 
             </Row>
-        </Fragment>
+        </Fragment >
     )
 }
 export default AboutUs
