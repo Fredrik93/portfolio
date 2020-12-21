1320cd3c16e1d470b618492006e8fd8477152aca
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-21 11:22:40 +0100

    add instagram

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,28 +13,6 @@ function Footer(props) {
         marginTop: "2vh"
 
     }
-    const darkOverlay = {
-        height: "25vh",
-        width: "100%",
-        background: "gray",
-        opacity: "95%",
-        overflow: "hidden"
-    }
-    const aboutUsImage = {
-        objectFit: "cover",
-        opacity: "0.4",
-        width: "100%",
-        height: "60vh"
-    }
-    const centeredText = {
-        position: "absolute",
-        top: "40%",
-        left: "50%",
-        transform: "translate(-50%,-50%",
-        border: "2px solid black"
-
-    }
-
 
     return (
         <Fragment>
@@ -46,6 +24,7 @@ function Footer(props) {
                         < br />
                         <a id="icons" target="_blank" href="https://github.com/Fredrik93"> <i className="fa fa-github" ></i> </a>
                         <a id="icons" target="_blank" href="https://www.linkedin.com/in/carl-fredrik-ullman-919645174/"  > <i className="fa fa-linkedin" ></i> </a>
+                        <a id="icons" target="_blank" href="https://www.instagram.com/ullmanitconsulting/"  > <i className="fa fa-instagram" ></i> </a>
                         <a id="icons" target="_blank" href={cv}  > <i className="fa fa-file"></i> </a>
 
                     </Col>
@@ -57,16 +36,6 @@ function Footer(props) {
                         <a className="links" > <li> Kontakt </li> </a>
                         <a className="links" > <li> Villkor </li> </a>
                     </Col>
-                    {/* <Col xs={12} sm={12} md={6} lg={4} style={{ marginTop: "4vh" }}   >
-                        <ul id="footer-links" style={{ listStyleType: "none", textAlign: "left", color: "black" }} >
-                            <h5 style={{ fontWeight: "bold" }} >Snabblänkar</h5>
-                            <Link className="links" to="/services" >  <li> Tjänster (Link Comp) </li>  </Link>
-                            <a className="links" href="/services" > <li> Tjänster </li> </a>
-                            <a className="links" href="/about-us" > <li> Om oss </li> </a>
-                            <a className="links" > <li> Kontakt </li> </a>
-                            <a className="links" > <li> Villkor </li> </a>
-                        </ul>
-                    </Col  > */}
                     <Col xs={12} sm={12} md={6} lg={4} id="footer-links" style={{ textAlign: "left", color: "black", marginTop: "4vh" }} >
                         <h5 style={{ fontWeight: "bold" }} > Information</h5>
                         <p  >
