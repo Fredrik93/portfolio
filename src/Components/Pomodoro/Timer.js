b4a5df11c8ba7e8241c3132b0189c0ae437a0765
Fredrik Ullman <fredrikullman93@gmail.com>
2020-12-25 08:50:28 +0100

    add grid to timer

--- src/Components/Pomodoro/Timer.js
+++ src/Components/Pomodoro/Timer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from '../Header'
 import clock from '../../Assets/clock.jpeg'
-import { Button } from 'react-bootstrap'
+import { Button, Row, Col } from 'react-bootstrap'
+import './Timer.css'
 
 class Timer extends React.Component {
     constructor() {
@@ -43,20 +44,25 @@ class Timer extends React.Component {
             < div >
                 <Header image={clock} headerText="Productivity Timer" />
                 <form style={{ textAlign: "center", marginTop: "4vh", marginBottom: "4vh" }} >
-                    <h3 > {this.state.timer} </h3>
-                    <h3>{this.state.minutes}:{this.state.seconds !== 59 ? this.state.seconds : "00"} </h3>
-                    <Button
-                        onChange={e => this.setState({ buttonClicked: true })}
-                        disabled={this.state.buttonClicked}
-                        style={{ margin: "2vh" }} variant="success" onClick={() => { this.setState({ intervalId: setInterval(this.handleChange, 1000) }) }} > Start Timer </Button>
-                    < br />
-                    <Button variant="danger" onClick={() => {
-                        clearInterval(this.state.intervalId)
-                        this.setState({
-                            timer: 1500, buttonClicked: false
-                        })
+                    <Row>
+                        <Col sm={12} md={12} lg={12} xlg={12} > <h3 > {this.state.timer} </h3>
+                            <h3 className="timer-component" >{this.state.minutes}:{this.state.seconds !== 59 ? this.state.seconds : "00"} </h3>
+                        </Col>
+                        <Col> <Button
+                            onChange={e => this.setState({ buttonClicked: true })}
+                            disabled={this.state.buttonClicked}
+                            style={{ margin: "2vh" }} variant="success" onClick={() => { this.setState({ intervalId: setInterval(this.handleChange, 1000) }) }} > Start Timer </Button>
+                        </Col>
+                        <Col> <Button variant="danger" onClick={() => {
+                            clearInterval(this.state.intervalId)
+                            this.setState({
+                                timer: 1500, buttonClicked: false
+                            })
 
-                    }} > Restart </Button>
+                        }} > Restart </Button>
+                        </Col>
+                    </Row>
+                    < br />
 
                 </form>
             </div >
