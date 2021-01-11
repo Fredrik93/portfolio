a2adc3d9cbe88752d04056b3cc1a595112ae46c4
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-11 08:00:43 +0100

    add  header text

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,47 +1,24 @@
 import React, { Fragment } from 'react'
-import vegetationImage from '../Assets/vegetation-1.jpg'
+import { Col, Row } from 'react-bootstrap'
+import vegetationImage from '../Assets/vegetation-2.jpg'
+import Header from './Header'
 import '../index.css'
 function AboutUs() {
-    const startPageLayout = {
-        position: "relative",
-        textAlign: "center",
-        color: "white",
 
-    }
-    const darkOverlay = {
-        height: "60vh",
-        width: "100%",
-        background: "#1A6C7A",
-        opacity: "95%",
-        overflow: "hidden"
-    }
-    const aboutUsImage = {
-        objectFit: "cover",
-        opacity: "0.4",
-        width: "100%",
-        height: "60vh"
-    }
-    const centeredText = {
-        position: "absolute",
-        top: "40%",
-        left: "50%",
-        transform: "translate(-50%,-50%",
-
-    }
     return (
         <Fragment>
+            <Header headerText="Om Oss" image={vegetationImage} />
+            <h4 style={{ textAlign: "center" }} >hi</h4>
 
-            <div style={startPageLayout} >
-                <header style={darkOverlay}>
-                    <img style={aboutUsImage} alt="homepage-vegetation" src={vegetationImage} />
-                    <section style={centeredText}>
-                        <h3> Services </h3>
+            <Row style={{ textAlign: "center" }} >
+                <Col>1</Col>
+                <Col>2</Col>
+                <Col>3</Col>
+            </Row>
 
-                    </section>
 
-                </header>
-            </div>
-        </Fragment>
+
+        </Fragment >
     )
 }
 export default AboutUs
