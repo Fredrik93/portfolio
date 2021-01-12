4719a2969626bddd0427871b10b7bad94b11101d
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-12 08:59:44 +0100

    center text

--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -4,15 +4,16 @@ import './Prices.css'
 function Prices(props) {
     return (
         <div id="price-card" >
-            <h3>  {props.title} {props.price} </h3>
-            < br />
-            {props.information}
-            <br />
-            <ul>
-                <li>{props.item1}</li>
-                <li>{props.item2}</li>
-            </ul>
-
+            <div id="price-text">
+                <h3>  {props.title} {props.price} </h3>
+                < br />
+                {props.information}
+                <br />
+                <ul>
+                    <li>{props.item1}</li>
+                    <li>{props.item2}</li>
+                </ul>
+            </div>
         </div>
     )
 } export default Prices
