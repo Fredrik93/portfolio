0d67fe9041f36dac075250ac85172b9fe8b3d7d9
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-13 07:56:43 +0100

    add styling price cards

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import vegetationImage from '../Assets/vegetation-2.jpg'
 import Header from './Header'
 import '../index.css'
@@ -13,41 +13,42 @@ function AboutUs() {
                 <h4 >våra produkter</h4>
                 <p> Vi erbjuder tre olika paket till riktigt bra priser.  </p>
             </div>
-            <Row  >
-                <Col className="d-flex justify-content-center" lg={4} md={12} sm={12} xs={12} >
-                    <Prices
-                        title="Basic"
-                        price="3000:-"
-                        information="Baspaketet. Detta är för dig som vill ha en rent 
-                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
-                        item1="Välj mellan ett 10 - tal mallar​"
-                        item2="3 sidor till hemsidan"
-                    />
-                </Col>
-                <Col className="d-flex justify-content-center" lg={4} md={12} sm={12} xs={12} >
-                    <Prices
-                        title="Medium"
-                        price="3000:-"
-                        information="Baspaketet. Detta är för dig som vill ha en rent 
-                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
-                        item1="Välj mellan ett 10 - tal mallar​"
-                        item2="3 sidor till hemsidan"
-                    />
-                </Col>
-                <Col className="d-flex justify-content-center" lg={4} md={12} sm={12} xs={12} >
-                    <Prices
-                        title="Professional"
-                        price="3000:-"
-                        information="Baspaketet. Detta är för dig som vill ha en rent 
-                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
-                        item1="Välj mellan ett 10 - tal mallar​"
-                        item2="3 sidor till hemsidan"
-                    />
-                </Col>
+            <Container>
+                <Row className="justify-content-center" >
 
-            </Row>
+                    <Col lg={4} md={12} sm={12} xs={12} >
+                        <Prices
+                            title="Basic"
+                            price="3000:-"
+                            information="Baspaketet. Detta är för dig som vill ha en rent 
+                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
+                            item1="Välj mellan ett 10 - tal mallar​"
+                            item2="3 sidor till hemsidan"
+                        />
+                    </Col>
+                    <Col lg={4} md={12} sm={12} xs={12} >
+                        <Prices
+                            title="Medium"
+                            price="3000:-"
+                            information="Baspaketet. Detta är för dig som vill ha en rent 
+                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
+                            item1="Välj mellan ett 10 - tal mallar​"
+                            item2="3 sidor till hemsidan"
+                        />
+                    </Col>
+                    <Col lg={4} md={12} sm={12} xs={12} >
+                        <Prices
+                            title="Professional"
+                            price="3000:-"
+                            information="Baspaketet. Detta är för dig som vill ha en rent 
+                      informativ hemsida med till exempel en startsida, en meny och en Om Oss sida."
+                            item1="Välj mellan ett 10 - tal mallar​"
+                            item2="3 sidor till hemsidan"
+                        />
+                    </Col>
 
-
+                </Row>
+            </Container>
 
         </Fragment >
     )
