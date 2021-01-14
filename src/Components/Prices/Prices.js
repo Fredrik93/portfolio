f30d1df1e4bb367aef5479773291424a65fa1fbf
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-14 13:25:24 +0100

    add overlay to pricecard

--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -2,9 +2,16 @@ import React from 'react'
 import './Prices.css'
 
 function Prices(props) {
-    return (
-        <div id="price-card" >
+    const darkOverlay = {
 
+
+        background: "#1A6C7A",
+        opacity: "95%",
+
+
+    }
+    return (
+        <div id="price-card" className="price-card" >
             <div id="price-text" >
                 <h3>  {props.title} {props.price} </h3>
                 < br />
