f4711a446ae3f309f0b247a2eae49913b8032a18
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-18 08:01:13 +0100

    add card

--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Prices.css'
 import vegImage from '../../Assets/vegetation-2.jpg'
+import { Container } from 'react-bootstrap'
 
 function Prices(props) {
     const darkOverlay = {
@@ -12,23 +13,24 @@ function Prices(props) {
 
     }
     return (
-        <div id="price-card" className="price-card" >
-            <div id="price-text" >
-                <section>
-                    <img id="image" src={vegImage} />
-                    <h3>  {props.title} {props.price} </h3>
-                    < br />
-                    {props.information}
-                    <br />
-                    <ul>
-                        <li>{props.item1}</li>
-                        <li>{props.item2}</li>
-                        <li>{props.item3}</li>
-                        <li>{props.item4}</li>
-                        <li>{props.item5}</li>
-                    </ul>
-                </section>
+        <Container id="container">
+            <div id="price-card" className="price-card" >
+                <div id="price-text" >
+                    <section>
+                        <h3>  {props.title} {props.price} </h3>
+                        < br />
+                        {props.information}
+                        <br />
+                        <ul>
+                            <li>{props.item1}</li>
+                            <li>{props.item2}</li>
+                            <li>{props.item3}</li>
+                            <li>{props.item4}</li>
+                            <li>{props.item5}</li>
+                        </ul>
+                    </section>
+                </div>
             </div>
-        </div>
+        </Container>
     )
 } export default Prices
