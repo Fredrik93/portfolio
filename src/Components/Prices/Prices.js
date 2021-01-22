bd7e0fcf1010b99fe0e74b30a086db8c2c54089d
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-22 07:17:16 +0100

    style cards

--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -2,41 +2,35 @@ import React from 'react'
 import './Prices.css'
 
 function Prices(props) {
-    const darkOverlay = {
 
-
-        background: "#1A6C7A",
-        opacity: "95%",
-
-
-    }
     return (
         <div>
-            <img src={props.image} id="container" />
-            <div id="price-card" className="price-card" >
-
-                <div id="price-text" >
+            <img src={props.image} id="container" alt="images-behind-pricing" />
 
 
-                    <h3>  {props.title} {props.price} </h3>
-                    < br />
-                    < br />
+            <div id="price-text" >
+                <h3 id="title" >  {props.title} {props.price} </h3>
+                < br />
+                < br />
+                <div id="information" >
                     {props.information}
-                    <br />
-                    <br />
-                    <ul id="items" >
-                        <li>{props.item1}</li>
-                        <li>{props.item2}</li>
-                        <li>{props.item3}</li>
-                        <li>{props.item4}</li>
-                        <li>{props.item5}</li>
-                        <li>{props.item6}</li>
-                        <li>{props.item7}</li>
-                        <li>{props.item8}</li>
-                    </ul>
-
                 </div>
+
+                <br />
+                <br />
+                <ul id="items" >
+                    <li>{props.item1}</li>
+                    <li>{props.item2}</li>
+                    <li>{props.item3}</li>
+                    <li>{props.item4}</li>
+                    <li>{props.item5}</li>
+                    <li>{props.item6}</li>
+                    <li>{props.item7}</li>
+                    <li>{props.item8}</li>
+                </ul>
+
             </div>
+
         </div>
     )
 } export default Prices
