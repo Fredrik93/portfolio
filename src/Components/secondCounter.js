82ba6a53cf043002e0f4a2e4b4eede4014387d8f
Fredrik Ullman <fredrikullman93@gmail.com>
2021-04-29 15:08:01 +0200

    button added

--- src/Components/secondCounter.js
+++ src/Components/secondCounter.js
@@ -2,23 +2,29 @@ import React from 'react'
 import { Fragment } from 'react';
 import Header from './Header';
 import inTheWoods from '../Assets/in-the-woods.jpg'
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Button, Container } from 'react-bootstrap';
+
 
 
 
 class secondCounter extends React.Component {
     render(){
+        var styling= {
+            textAlign:"center", marginTop:"10vh"
+        }
+        var count = 1;
         return(
             <Fragment>
             <Header headerText="Second Counter" image={inTheWoods} />
-        
-              
-                   
-                    <div style={{textAlign:"center"}} >
-                        Counter: 
-                                       </div>
-                  
-                
+            <Container>
+           <div style={styling} >
+               Counter: {count}
+               </div>
+               <Button style={styling} variant="secondary" onClick={()=>{console.log("clicked")}} >Click dis</Button>    
+
+               </Container>
+               
+{/* https://mdbootstrap.com/docs/react/utilities/flexbox/ */}
       
 
             </Fragment>
