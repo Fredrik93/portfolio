2e7be437f69f93604caced6ab17e1242915a092e
Fredrik Ullman <fredrikullman93@gmail.com>
2021-07-10 14:03:01 +0200

    hook up api

--- src/Components/PlantsAPI.js
+++ src/Components/PlantsAPI.js
@@ -3,14 +3,49 @@ import { useEffect, useState } from 'react';
 
 
 function PlantsAPI () {
+  const [isLoading, setIsLoading] = useState(true);
+  const [plants, setPlants] = useState([])
 
+useEffect(()=>{
+  fetch('http://192.168.1.23:8080/plants')
+  .then(response => response.json())
+  .then((data)=>{
+    setPlants(data)
+  })
 
+},[])
 
-
-
-
+  const el = 
+    plants.map((elem)=>{
+    return(  
+  
+      <table key={elem+"table"} style={{ border:"1px solid black", width:"55vh", display:"flex", justifyContent:"center"}}  >
+        <tbody   >
+  <tr  >
+    <th >Name</th>
+  </tr>
+  <tr  >
+    <td >{elem}</td>
+  </tr>
+  </tbody>
+</table>
+      )
+    
+      
+    })
+  
  return (
-   <h1 style={{marginTop:"20vh", textAlign:"center"}} > {  " and temp is:  " + 0}  </h1>
+  <div style={{marginTop:"20vh", textAlign:"center"}} >
+  
+ 
+   <h1> API calls with React Hooks </h1>
+    {el}
+
+ 
+
+  
+      
+ </div>
  )
 }
 
